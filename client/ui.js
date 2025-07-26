7c1b37e8bdeb4395a6347491e021268b060133ee
sztego <sztego@gmail.com>
2025-07-26 11:34:38 +0200

    button placements

--- client/ui.js
+++ client/ui.js
@@ -4,6 +4,14 @@ function selectBuilding(type) {
     selectBuilding = type;
 }
 
-function saveBase() {
+function newGame(){
+    socket.emit("newBase", buildings)
+}
+
+function saveGame() {
     socket.emit("saveBase", buildings);
+}
+
+function loadGame(){
+    socket.emit("loadBase", buildings);
 }
