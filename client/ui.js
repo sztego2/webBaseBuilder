aa1d84eabe56acac050280d19f6067808c664167
sztego <sztego@gmail.com>
2025-07-27 01:30:59 +0200

    buildingAdd

--- client/ui.js
+++ client/ui.js
@@ -1,17 +1,56 @@
-const socket = io();
+//const socket = io();
+//numbers
+let houses = 2;
+let powerplants = 1;
+let farms = 1;
+let factories = 0;
+let barracks = 0;
 
-function selectBuilding(type) {
-    selectBuilding = type;
-}
 
-function newGame(){
-    socket.emit("newBase", buildings)
+//menu
+
+/* function newGame(){
+    socket.emit("newBtn", buildings);
 }
 
 function saveGame() {
-    socket.emit("saveBase", buildings);
+    socket.emit("saveBtn", buildings);
 }
 
 function loadGame(){
-    socket.emit("loadBase", buildings);
+    socket.emit("loadBtn", buildings);
+} */
+
+//builders
+function addBuilding(buildingType){
+    switch(buildingType){
+        case "house":
+            houses++;
+            updateOutput("quantityHouse", houses);
+            break;
+        case "powerplant":
+            powerplants++;
+            updateOutput("quantityPowerplant", powerplants);
+            break;
+        case "farm":
+            farms++;
+            updateOutput("quantityFarm", farms);
+            break;
+        case "factory":
+            factories++;
+            updateOutput("quantityFactory", factories);
+            break;
+        case "barracks":
+            barracks++;
+            updateOutput("quantityBarracks", barracks);
+            break;
+    }  
 }
+
+function updateOutput(id, newValue){
+    const element = document.getElementById(id);
+    element.value = newValue;
+}
+
+
+
